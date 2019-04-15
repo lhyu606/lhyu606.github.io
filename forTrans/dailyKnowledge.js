150f89f086823d5d46bbf6e2951dd573a27be176
A_pupil <A_pupil@163.com>
2019-04-15 14:30:21 +0800

    some dailyKnowledge

---
+++ forTrans/dailyKnowledge.js
@@ -0,0 +1,273 @@
+// https://juejin.im/post/5cab0c45f265da2513734390
+
+// 原始类型 栈内存储 存储的是 【值】
+// string boolean number undefined null symbol
+// 引用类型 堆内存储 存储的是 【地址】
+// Object Array Function
+
+
+// typeof 可判断 string boolean number function Object【包括 null Object Array】
+// instanceof 通过原型链判断
+
+
+// for of 需要具有 iterater 接口
+// for in 需要具备 可枚举 属性 enumerable 
+// forEach 只能遍历数组，不可中断，无返回值或 undefined
+// map 只能遍历数组，不可中断，返回修改后的数组
+
+
+// 判断 数组
+// Array.isArray( arr );
+// arr isinstanceof Array
+// Object.prototype.toString.call( arr ) === '[Object Array]'
+// arr.constructor == Array (不准确，arr.constructor 可指定其他)
+
+
+// 类数组 和 数组 区别 具有 length 不具有其他数组方法
+// 常见 类数组：函数参数 arguments、 DOM 列表（如: document.querySelectorAll()）、$('.div');
+// 类数组 转换成 数组 
+// Array.prototype.slice.call( arguments )
+// [...arguments]
+// Array.from(arguments)
+// 任何定义了 iterater 接口的对象，都可以扩展成真正的数组
+
+
+// == 和 === 
+// === 只有 同类型 且 值 相等，才返回 true
+// == 默认转换成同类型，然后 进行 值 的比较
+// null undefined 转换 boolean
+// string 和 number 转成 number
+// boolean 和 其他 转成 boolean
+
+// 引用类型 转换 Boolean 都是 true
+
+// [] == ![] => true ：
+// 1、 ![] 优先级 高 ![] => false
+// 2、 [] == false => [] 转换为 Number 0; 
+// 3、 0 == false => true
+
+
+// ES5 和 ES6 class 区别
+// es6 class 内部所有定义的方法都是不可枚举的
+// es6 必须使用 new 调用
+// es6 不存在变量提升
+// es6 默认既是严格模式
+// es6 子类必须在父类的构造函数中调用 super() ，才有 this 对象，
+// es5 类继承关系是相反的，先有子类 this ，然后父类方法应用在子类上
+
+
+// 改变原数组 api
+// splice reverse fill copyWithin sort push pop shift unshift
+
+// 不改变原数组 API
+// slice map forEach every filter reduce entry entries find
+
+
+// let 局部变量，不提升，不允许重复声明
+// const 局部常量，不提升，不允许重复声明，不允许改变
+// 暂时性死区
+
+
+// this 指向
+// 四种绑定：默认，隐式，显示， new 
+// call 枚举式
+// apply 数组形式
+// bind 返回一个函数，该函数里 return apply（context, args）
+// => 函数指向上一层 this 
+
+
+// 闭包
+// 受保护变量，返回操作函数，【私有变量，模仿块级作用域】
+
+function mynew(func) {
+	let target = {};
+	target.__proto__ = func.prototype;
+	let res = func.call(target);
+	if (typeof(res) == 'object' || typeof(res) == 'function') {
+		return res;
+	}
+	return target;
+}
+
+
+// 原型链
+console.log(Object.prototype.__proto__ == null) // true
+console.log(Object.prototype.__proto__) // null
+console.log(Object.prototype) // {}
+
+var obj = new Object();
+obj.__proto__ == Object.prototype  // true
+
+// es5 实现继承
+function Super(name, age) {
+	this.name = name;
+	this.age = age;
+}
+Super.prototype.say = function() {
+	console.log(this.name);
+}
+
+function Sub(name, age) {
+	Super.call(this, name);
+	this.age = age;
+}
+Sub.prototype = new Super();
+Sub.prototype.constructor = Sub;
+
+Sub.prototype.sayAge = function() {
+	console.log(this.age);
+}
+
+
+// 深 copy
+function deepCopy(obj) {
+	if (obj == null) {
+		return null;
+	}
+	if(obj instanceof Date || obj instanceof RegExp) {
+		return new Date(obj);
+	}
+	// let t = new obj.constructor();
+	var type = Object.prototype.toString.call(obj).slice(8, -1);
+	// console.log(type == 'Array' || type == 'Object')
+	if(type == 'Array' || type == 'Object') {
+		for(let key in obj) {
+			deepCopy(obj[key])
+		}
+	}
+
+	return obj;
+}
+
+var obj = {
+	name: 'n',
+	age: 1,
+	date: new Date(),
+	arr: [1, 2],
+	obj: {
+		n: 'n',
+		a: 2,
+		arr: ['1', [1,2,3], {nn: '5'}]
+	},
+	func: function() {
+		console.log(this.name);
+	}
+}
+var obj2 = deepCopy(obj)
+console.log(obj2)
+
+
+// 防抖
+function debounce(fun, wait) {
+	let timer;
+	let later = () => {
+		timer = setTimeout(() => {
+			fun();
+		}, wait);
+	}
+	function deb() {
+		if(!timer) {
+			later();
+		} else {  console.log('later')
+			clearTimeout(timer);
+			timer = null;
+			later();
+		}
+	}
+	return deb;
+}
+
+let n = 0;
+function clk() {
+	console.log('click  ' + n++)
+}
+// document.onclick = debounce(clk, 500);
+
+// 节流
+function throttle(fun, wait) {
+	let timer = null;
+	let flag = false;
+
+	return () => {
+		if(!flag) {
+			flag = true;
+			fun();
+			clearTimeout(timer);
+			timer = setTimeout(() => {
+				flag = false;
+			}, wait);
+		}
+	}
+}
+let m = 0;
+function move(){
+	console.log('move ' + m++);
+}
+
+// document.onmousemove = throttle(move, 500);
+
+
+// 最大值
+Math.max.apply(null, [14, 2, 6, 3, 5]);
+Math.max(...[14, 2, 6, 3, 5]);
+
+
+// setTimeout 第二参数 表示 最少时间， 不是确切时间，且不能低于 4 毫秒，否则为 4 毫秒；老浏览器 10 毫秒
+
+
+// promise 三种状态： pending fulfilled rejected
+
+// promise 是微任务， setTimeout 是宏任务，所以 promise 先执行
+// macro-task(宏任务)：setTimeout、 setInterval、 setImmediate
+// micro-task(微任务)：Promise、 process.nextTick
+
+
+// 函数柯里化
+
+
+// 自定义事件   fn1.trigger('event', args);  fn1.on('event', cb(args));  
+
+
+// 模块化解决：命名冲突、文件依赖、代码复用
+
+
+// parseInt(string, radix) //string 被处理的值，radix 解析时的基数（进制，string 不以 0x  0  开头的 10 进制处理）
+// 故 ['1', '2', '3'].map(parseInt) // ==> [1, NaN, NaN]
+
+{}
+
+var b = 10;
+(function b() {
+  b = 20;
+  console.log(b)
+})()		// 
+// 输出：
+// ƒ b() {
+//   b = 20;
+//   console.log(b)
+// }
+
+// var c = 10;
+// (function c() {
+//   'use strict'
+//   c = 20;		// error in here
+//   console.log(c)
+// })()				// TypeError: Assignment to constant variable.
+
+
+var a = {
+	i: 1,
+	toString: function () {
+	// valueOf: function () {
+		return this.i++;
+	}
+};
+if(a==1 && a==2 && a==3) {
+	console.log(1)
+}
+
+var ar = [1,2,3];
+ar.join = ar.shift;
+if (ar==1 && ar==2 && ar==3) {
+	console.log(ar) // [join: ƒ]
+}
---
+++ forTrans/vueQuestions.js
@@ -0,0 +1,147 @@
+// 问题集合
+// 组件 data 由于 JavaScript 对象是引用类型，所以需要一个立即执行函数返回一个全新的 data 对象为该组件独享，避免同一组件多处引用时造成数据共享
+
+
+// computed 和 watch 都是用于监听数据变化
+// computed 声明 的 函数名【comp】 机作为 vm data 的一部分，组件内可以直接 this.comp 引用，用于监听所依赖的 数据（一个或多个）变化， 简单处理，返回新值
+
+// watch 监听 单个数据变化，适合较 computed 更复杂的处理，进行 data 更新
+
+
+// 组件间通信：
+// 1、 父子间通信： 子【 $emit('event', args); 】, 父【 $on('event', function(args) { /* dosomething */ }; 】
+
+// 2、 非父子间通信： eventBus
+
+// 3、 vuex 【 state、 mutation、 action、 getter、 module 】
+
+// 新增变量：全局 vm.set(this.data, 'foo', value);  组件 this.$set(this.data, 'foo', val);
+
+// v-if 用于不经常切换的元素，控制是否渲染
+// v-show 用于经常切换的元素，控制 display：block | none;
+
+// 列表渲染时，添加 v-key='yourkey'，vue 识别节点，利于 diff 算法，列表简单时 可以不加，“就地复用”策略【默认】	....还有更多
+
+// 路由懒加载
+// const getComponent = (name, component) => () => import(`@/view/${name}/{component}.vue`);
+// const myRouter = new Router({
+	// routes: [
+		// {
+			// path: '/',
+			// name: 'home',
+			// component: getComponent('login', 'index')
+		// }
+		// ....
+	// ]
+// });
+
+// 进度条插件 nprogress
+
+// 导航守卫，验证 token
+myRouter.beforeEach( (to, from, next) => {
+	NProgress.start();
+	if (to.path !== '/login' && !store.state.token) {
+		next('/login');
+		NProgress.done();
+	}
+
+	next();
+} );
+
+myRouter.afterEach( () => {
+	NProgress.done();
+} );
+
+
+import axios from 'axios';
+import router from '../router/router';
+import { Loading } from 'element-ui'; // element-ui
+
+// axios 基本设置
+axios.default.timeout = 60000;
+axios.default.baseURL = process.env.BASE_URL;
+axios.default.headers.post['Content-type'] = "application/x-www-form-urlencoded;charset=UTF-8";
+let loading = null; // 初始化 loading
+
+// 请求拦截 【请求前处理】
+axios.intercetors.request.use(
+	config => {
+		loading = Loading.service({
+			text: '正在加载中....',
+			follscreen: true
+		});
+		if (store.state.token) {
+			config.headers.['Authorization'] = 'Bearer' + store.state.token;
+		}
+		return config;
+	},
+	error => {
+		return Promise.reject(error);
+	}
+);
+// 响应拦截 【数据返回处理】
+axios.intercetors.response.use(
+	response => {
+		return new Promise( (resolve, reject) => {
+			// 成功后关闭加载框
+			if (loading) {
+				loading.close();
+			}
+			const res = response.data;
+			if (res.err_code === 0) {
+				resolve(res);
+			} else {
+				reject(res);
+			}
+		} );
+	},
+	error => {
+		console.log(error);
+		// 请求成功后关闭加载框
+		if (loading) {
+			loading.close();
+		}
+		// 断网处理货请求超时
+		if (!error.response) => {
+			// 请求超时
+			if (error.message.include('timeout')) {
+				console.log('请求超时');
+				messages('error', '请求超时，请检查互联网链接');
+			} else {
+				// 断网，可以展示断网组件
+				console.log('断网了');
+				messages('error', '请检查网络是否已连接');
+			}
+			return ;
+		}
+		const status = error.response.status;
+		switch (status) {
+			case 500: 
+				messages('error', '服务器内部错误');
+				break;
+			case 404: 
+				messages('error', '未找到远程服务器');
+				break;
+			case 401: 
+				messages('warning', '用户登录已过期，请重新登录');
+				localStorage.removeItem('token');
+				setTimeout( () => {
+					router.replace({
+						path: '/login',
+						query: {
+							redirect: router.currentRoute.fullPath
+						}
+					});
+				}, 1000);
+				break;
+			case 400: 
+				messages('error', '数据异常');
+				break;
+			default: 
+				messages('error', error.response.data.message);
+		}
+		return Promise.reject(error);
+	}
+);
+
+// vuex-persistedstate   vuex 数据持久化插件
